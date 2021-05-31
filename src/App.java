4c108ec1005501719ce004ffc3189e9f109fcf43
faithrider <85044320+faithrider@users.noreply.github.com>
2021-05-30 20:09:08 -0400

    Better UI

--- src/App.java
+++ src/App.java
@@ -5,8 +5,8 @@ public class App {
 
     public static boolean flip(String guess){
         //tails equals 1 and heads equals 2
-        int result = (int)(Math.random() * 2 + 1);
-        if((result == 2 && guess.equals("heads")) || (result == 1 && guess.equals("tails")))
+        int result = (int)((Math.random() * 2) + 1);
+        if((result == 1 && guess.equals("heads")) || (result == 2 && guess.equals("tails")))
             return true;
         return false;
 
@@ -15,14 +15,13 @@ public class App {
         Scanner scan = new Scanner(System.in);
         scan.useDelimiter(System.lineSeparator());
 
-        String cont = "yes";
         boolean ans;
         int compC = 0;
         int playC = 0;
 
         System.out.println("What's your given name?");
         String firstName = scan.nextLine();
-        String nick = firstName.substring(0,1);
+        String nick = (firstName.substring(0,1).toUpperCase());
 
         System.out.println("And your surname?");
         String lastName = scan.nextLine();
@@ -33,38 +32,58 @@ public class App {
         int age = scan.nextInt();
 
         System.out.print("Hmm...I think " + age + " is old enough to play my little game. Say, " + nick + ", can I call you " + nick + "? ");
-        System.out.println("My favorite game is a good old-fashioned coin flip! I'll keep score—you can trust me *wink wink*.");
+        System.out.println("My favorite game is a good old-fashioned coin flip! I'll keep score——you can trust me *wink wink*.");
 
-        String guess = "";
+        System.out.println("So! Heads or tails? (H for heads / T for tails / Q to quit)");
+        String guess = scan.next();
+        guess = guess.toUpperCase();
 
-        while(cont.equals("yes")){
-            System.out.println("Heads or tails? (all lowercase please!)");
-            guess = scan.next();
-            ans = flip(guess);
+        if(guess.equals("H") || guess.equals("T")){
+            while(guess.equals("H") || guess.equals("T")){
             
-            if(ans){
-                playC++;
-                System.out.println("Darn! It WAS " + guess + "! Play again? yes/no");
-                System.out.println("\nScore: [Computer - " + compC + " ]  [Player - " + playC + " ]");
-                cont = scan.next();
+                if(guess.equals("H")){
+                    guess = "heads";
+                }
+                else if(guess.equals("T")){
+                    guess = "tails";
+                }
+                ans = flip(guess);
+                
+                if(ans){
+                    playC++;
+                    System.out.println("Darn! It WAS " + guess + "! Heads or tails? (H/T/Q)");
+                    System.out.println("\nScore: [Computer - " + compC + " ]  [Player - " + playC + " ]");
+                    guess = scan.next();
+                    guess = guess.toUpperCase();
+                }
+                else if(!ans){
+                    compC++;
+                    System.out.println("Bazinga! I KNEW it wouldn't be " + guess + "! Heads or tails? (H/T/Q)");
+                    System.out.println("\nScore: [Computer - " + compC + " ]  [Player - " + playC + " ]");
+                    guess = scan.next();
+                    guess = guess.toUpperCase();
+                }
             }
-            else if(!ans){
-                compC++;
-                System.out.println("Bazinga! I KNEW it wouldn't be " + guess + "! Play again? yes/no");
-                System.out.println("\nScore: [Computer - " + compC + " ]  [Player - " + playC + " ]");
-                cont = scan.next();
+            if(guess.equals("Q")){
+                if(playC > compC){
+                    System.out.println("Man, I really thought I had it, " + nick + "! GG!");
+                }
+                else if(playC < compC){
+                    System.out.println("Well...not everyone is as great as me, " + nick + ". Better luck next time!");
+                }
+                else if(playC == compC){
+                    System.out.println("Did you end then on purpose " + nick + "?? I guess we tie...Let's play again soon!");
+                }
+            }
+            else{
+                System.out.println("That wasn't——you know what, take your jokes somewhere else, " + nick + ".");
             }
         }
-        if(playC > compC){
-            System.out.println("Man, I really thought I had it, " + nick + "! GG!");
+        else{
+            System.out.println("Really? If you want to play later, come find me.");
         }
-        else if(playC < compC){
-            System.out.println("Well...not everyone is as great as me, " + nick + ". Better luck next time!");
-        }
-        else if(playC == compC){
-            System.out.println("Did you end then on purpose " + nick + "?? I guess we tie...Let's play again soon!");
-        }
-
+        
+        System.out.println("*\n*\n*\nEND");
         scan.close();
     }
 }
